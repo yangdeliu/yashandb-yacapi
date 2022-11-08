f9b185425bac319b9c702379d6e7eff63297e8e1
wuliangzhi <wulianglzhi@sics.ac.cn>
2022-11-08 12:13:59 +0800

    fix:win 编译告警

---
+++ src/yapi_infr.c
@@ -0,0 +1,19 @@
+#include "yapi_inc.h"
+#include <inttypes.h>
+
+YapiResult yapiAllocMem(const char* name, size_t numMembers, size_t memberSize, void **ptr, YapiErrorMsg *error)
+{
+    size_t size = numMembers * memberSize;
+    *ptr = malloc(size);
+    if (!*ptr){
+        yapiSetError(error, YAPI_ERR_ALLOC_MEM, "cannot allocate %" PRId64 " bytes for %s", size, name);
+        return YAPI_ERROR;
+    }
+    return YAPI_SUCCESS;
+}
+
+//-----------------------------------------------------------------------------
+void YapiFreeMem(void *ptr)
+{
+    free(ptr);
+}
